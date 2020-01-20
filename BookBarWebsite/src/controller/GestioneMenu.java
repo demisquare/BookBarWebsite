a7a8407a03c98b6650a35c76277586c7a25e0e38
Paola Guarasci <paolaguarasci@protonmail.ch>
2020-01-20 23:17:37 +0100

    fix

--- BookBarWebsite/src/controller/GestioneMenu.java
+++ BookBarWebsite/src/controller/GestioneMenu.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-@WebServlet(value = "/gestione_menu")
 @SuppressWarnings("serial")
 
 public final class GestioneMenu extends HttpServlet {
